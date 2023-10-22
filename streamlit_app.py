1d82b192843256f3e24e3372f14b5abec7f36219
Awkword00 <Awkword6@gmail.com>
2023-10-22 23:16:18 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -79,7 +79,4 @@ if st.button('Получи ответ на множество вопросов!'
       result[i] = "Да"
     else:
       result[i] = "Нет"
-  st.write(pd.DataFrame(list(zip(st.session_state.df["Вопрос"], result)), columns=["Вопрос","Ответ"])
-
-
-
+  st.write(pd.DataFrame(list(zip(st.session_state.df["Вопрос"].tolist(), result)), columns=["Вопрос","Ответ"])
