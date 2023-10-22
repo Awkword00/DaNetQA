05744f95edbfefebf902e424c0feddcc92c48dfb
Awkword00 <Awkword6@gmail.com>
2023-10-22 22:31:25 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,7 +64,7 @@ if st.session_state.answer_ready == True:
         st.write("# Нет!")
 
 data = pd.DataFrame(columns = ["Текст","Вопрос"])
-session_state = SessionState.get(df=data
+session_state = SessionState.get(df=data)
 if st.button("Добавить новую строку"):
     session_state.df = session_state.df.append(["",""], ignore_index=True)
     st.dataframe(session_state.df)
