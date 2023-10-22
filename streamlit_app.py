47e8023c244dee5a3af0a125dcf442338d412c03
Awkword00 <Awkword6@gmail.com>
2023-10-22 23:11:16 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -74,7 +74,6 @@ if 'df' not in st.session_state:
 st.session_state.df = st.experimental_data_editor(st.session_state.df,num_rows="dynamic") 
 
 st.write(st.session_state.df)
-st.session_state.df = data
 if st.button('Получи ответ на множество вопросов!'):
   result = model_DaNetQA.model_answer(st.session_state.df["Вопрос"].tolist(), st.session_state.df["Текст"].tolist())
   for i in range(len(result)):
